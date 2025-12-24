23546d63fb387f00103be7070f0518f09697b915
Ishika-45 <bansalishika58@gmail.com>
2025-12-24 21:24:19 +0530

    Check server connectivity

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,25 @@ import 'dotenv/config'
 import userRoute from './routes/userRoute.js'
 import connectDB from './config/mongodb.js'
 import chatRouter from './routes/chatRoute.js'
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
 const app=express()
 const port=process.env.PORT || 4000
+
 connectDB();
+
+connectCloudinary();
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/api/admin',adminRouter)
 app.use('/api/user',userRoute)
 app.use('/api/chat',chatRouter)
 
 app.get('/',(req,res)=>{
-    res.send('Server is running great')
+    res.send('Server is running OK!!!')
 })
 
 app.listen(port,()=> console.log("Server Started on",port))
