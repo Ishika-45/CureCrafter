8d0ad4d1c3908300bf90bc5e427a6e754437aed5
Ishika-45 <bansalishika58@gmail.com>
2025-12-24 21:26:01 +0530

    Make logic for admin login

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,80 @@
+import validator from 'validator';
+import bcrypt from 'bcryptjs';
+import {v2 as cloudinary} from 'cloudinary';
+import doctorModel from '../models/doctorModel.js';
+import jwt from 'jsonwebtoken';
+
+const addDoctor = async (req, res) => {
+    try {
+        const {name, email, password, speciality, degree, experience, about, fees, address } = req.body;
+        const imageFile = req.file;
+
+        if(!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address){
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+
+        if(!imageFile){
+            return res.status(400).json({ message: 'Image file is required' });
+        }
+
+        if(!validator.isEmail(email)){
+            return res.status(400).json({ message: 'Invalid email format' });
+        }
+
+        if(password.length < 8){
+            return res.status(400).json({ message: 'Password must be at least 8 characters long' });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+            resource_type: "  image",
+            // folder: 'doctor_images',
+        });
+        const imageUrl = imageUpload.secure_url;
+
+        const doctorData = {
+            name,
+            email,
+            image: imageUrl,
+            password: hashedPassword,
+            speciality,
+            degree,
+            experience,
+            about,
+            fees,
+            address: JSON.parse(address),     //convert object to string
+            date: Date.now(),
+        };
+
+        const newDoctor = new doctorModel(doctorData);
+        await newDoctor.save();
+        res.status(201).json({ message: 'Doctor added successfully', doctor: newDoctor });
+    }
+
+    catch (error) {
+        console.error('Error adding doctor:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+}
+
+const loginAdmin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const adminEmail = process.env.ADMIN_EMAIL;
+        const adminPassword = process.env.ADMIN_PASSWORD;
+        if (email === adminEmail && password === adminPassword) {
+            const token = jwt.sign(email+password, process.env.JWT_SECRET);
+            res.status(200).json({ token }); 
+        }
+        else {
+            res.status(401).json({ message: 'Invalid admin credentials' });
+        }
+    } catch (error) {
+        console.error('Error logging in admin:', error);
+        res.status(500).json({ message: 'Server error' });
+    }   
+}
+
+export { addDoctor, loginAdmin};
